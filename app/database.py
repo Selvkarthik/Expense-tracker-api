b5b62632da00d5254d8e89c19dad8ff354e8682c
Selvkarthik <125003223+Selvkarthik@users.noreply.github.com>
2026-08-13 13:23:00 +0530

    Alembic Migrations

--- app/database.py
+++ app/database.py
@@ -1,11 +1,18 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, URL
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-DB_URL = os.getenv('DB_URL')
+DB_URL = URL.create(
+    drivername = "postgresql+psycopg2",
+    username = os.getenv('DB_USER'),
+    password = os.getenv("DB_PASSWORD"),
+    host = os.getenv("DB_HOST"),
+    port = os.getenv("DB_PORT"),
+    database = os.getenv("DB_NAME"),
+)
 
 class Base(DeclarativeBase):
     pass
--- app/main.py
+++ app/main.py
@@ -4,8 +4,6 @@ from . import models
 
 app = FastAPI(title='Expense Tracker API')
 
-models.Base.metadata.create_all(bind=engine)
-
 @app.get('/')
 def root():
     return {"Message" : "App running successfully"}
